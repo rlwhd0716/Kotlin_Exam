d8af740baec998f39082401c23955a3f6e8e7a3d
rlwhd0716 <kfs08@naver.com>
2019-10-14 14:32:43 +0900

    2019-10-11 updated by rlwhd0716 poi 설명 커스텀

--- app/src/main/java/kr/com/rlwhd/kotlinexample/example/TiltSensorActivity.kt
+++ app/src/main/java/kr/com/rlwhd/kotlinexample/example/TiltSensorActivity.kt
@@ -1,13 +1,53 @@
 package kr.com.rlwhd.kotlinexample.example
 
+import android.content.Context
+import android.content.pm.ActivityInfo
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.WindowManager
 import kr.com.rlwhd.kotlinexample.R
 
-class TiltSensorActivity : AppCompatActivity() {
+class TiltSensorActivity : AppCompatActivity(), SensorEventListener {
+    private val TAG: String = this.javaClass.simpleName
+    private val sensorManager by lazy {
+        getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
+    private lateinit var tiltView: TiltView
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(this,
+            sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+            SensorManager.SENSOR_DELAY_NORMAL)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tilt_sensor)
+
+        tiltView = TiltView(this)
+        setContentView(tiltView)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        event?.let {
+            Log.d(TAG, "onSensorChanged - x : ${event.values[0]}, y : ${event.values[1]}, z : ${event.values[2]}")
+
+            tiltView.onSensorEvent(event)
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
     }
 }
